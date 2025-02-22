d4bee9c92393f7a438b042c50d8a9ce54bbe90b7
PLAIS Lionel <lionel.plais@ilp-web.net>
2025-02-22 18:06:30 +0100

    Version fonctionnelle

--- BaseFrance.py
+++ BaseFrance.py
@@ -1,25 +1,180 @@
 #!/bin/python3
 import os
+import googlemaps.client
+import googlemaps.elevation
+import requests
+from colorama import Fore, Style
+import gzip
 import sqlite3
-import csv
+import pandas
+from dotenv import load_dotenv
+import googlemaps
+from tqdm import tqdm
+
+# Charger les variables d'environnement
+load_dotenv(".env")
+
+# Initialiser le client Google Maps
+gmaps = googlemaps.Client(key = os.getenv("GOOGLE_API_KEY"))
+
+# Données à traiter
+scope = os.getenv("SCOPE")
+
+def recupCSV():
+	"""
+	Récupère le fichier CSV des adresses de toute la France.
+	"""
+	# Télécharger le fichier CSV
+	url = "https://adresse.data.gouv.fr/data/ban/adresses/latest/csv/adresses-{scope}.csv.gz"
+
+	print(Fore.BLUE + "[*] Téléchargement du fichier CSV..." + Style.RESET_ALL)
+
+	response = requests.get(url)
+	with open(f"adresses-{scope}.csv.gz", "wb") as f:
+		f.write(response.content)
+
+	print(Fore.GREEN + "[+] Fichier CSV téléchargé avec succès." + Style.RESET_ALL)
+
+	print(Fore.BLUE + "[*] Décompression du fichier CSV..." + Style.RESET_ALL)
+
+	# Décompresser le fichier CSV
+	with gzip.open(f"adresses-{scope}.csv.gz", "rb") as f:
+		csv_data = f.read()
+
+	# Enregistrer le fichier CSV décompressé
+	with open(f"adresses-{scope}.csv", "wb") as f:
+		f.write(csv_data)
+
+	print(Fore.GREEN + "[+] Fichier CSV décompressé avec succès." + Style.RESET_ALL)
 
 def creerBDD():
 	"""
 	Créer la base de données pour les adresses de toute la France.
 	"""
+	# Créer la base de données
+	print(Fore.BLUE + "[*] Création de la base de données..." + Style.RESET_ALL)
+	conn = sqlite3.connect(f"adresses-{scope}.sqlite")
+	cursor = conn.cursor()
 
+	# Créer la table des villes
+	cursor.execute("""
+	CREATE TABLE IF NOT EXISTS VILLES (
+		VIL_CODE_INSEE    INTEGER PRIMARY KEY,
+		VIL_NOM           TEXT NOT NULL,
+		VIL_CODE_POSTAL   TEXT NOT NULL
+	);
+	""")
+
+	# Créer la table des adresses
+	cursor.execute("""
+	CREATE TABLE IF NOT EXISTS ADRESSES (
+		ADR_ID              TEXT PRIMARY KEY,
+		ADR_NUMERO          INTEGER NOT NULL,
+		ADR_REP             TEXT,
+		ADR_NOM_VOIE        TEXT NOT NULL,
+		ADR_LATITUDE        REAL,
+		ADR_LONGITUDE       REAL,
+		ADR_ALTITUDE        REAL,
+		ADR_VIL_CODE_INSEE  INTEGER NOT NULL,
+		FOREIGN KEY (ADR_VIL_CODE_INSEE)
+			REFERENCES VILLES (VIL_CODE_INSEE) 
+				ON DELETE CASCADE 
+				ON UPDATE CASCADE
+	);
+	""")
+
+	# Valider les modifications
+	conn.commit()
+
+	# Fermer la connexion et le curseur
+	cursor.close()
+	conn.close()
+
+	print(Fore.GREEN + "[+] Base de données créée avec succès." + Style.RESET_ALL)
 
 def remplirBDD():
 	"""
 	Remplit la base de données avec les adresses contenues dans le fichier CSV.
 	"""
+	# Ouvrir la connexion à la base de données
+	conn = sqlite3.connect(f"adresses-{scope}.sqlite")
+	cursor = conn.cursor()
 
-def recupAltitude():
+	# Lire le fichier CSV en utilisant pandas
+	print(Fore.BLUE + "[*] Lecture du fichier CSV..." + Style.RESET_ALL)
+	donneesCSV = pandas.read_csv(f"adresses-{scope}.csv", sep = ";", encoding = "utf-8", index_col = "id")
+
+	# Nombre total d'adresses
+	print(Fore.BLUE + "[*] Comptage des adresses..." + Style.RESET_ALL)
+	nombre_adresses = len(donneesCSV)
+	print(Fore.GREEN + f"[+] Nombre d'adresses : {nombre_adresses:_}.".replace("_", " ") + Style.RESET_ALL)
+
+	print(Fore.BLUE + "[*] Remplissage de la base de données..." + Style.RESET_ALL)
+
+	# Initialiser la ville en cours
+	ville_en_cours = None
+
+	# Parcours les données du fichier CSV
+	for ligne in tqdm(donneesCSV.itertuples(),
+			desc = "Remplissage de la base de données",
+			unit = " adresses",
+			total = nombre_adresses):
+		# Récupérer les données de la ligne
+		adr_id = ligne.Index
+		adr_numero = int(ligne.numero)
+		adr_rep = ligne.rep
+		adr_nom_voie = ligne.nom_voie
+		vil_code_postal = ligne.code_postal
+		vil_code_insee = int(ligne.code_insee)
+		vil_nom = ligne.nom_commune
+		adr_longitude = float(ligne.lon)
+		adr_latitude = float(ligne.lat)
+
+		# Récupérer l'altitude
+		adr_altitude = recupAltitude(adr_latitude, adr_longitude)
+
+		# Insérer les données dans la table des villes si elles n'existent pas
+		cursor.execute("""
+			INSERT OR IGNORE INTO VILLES (VIL_CODE_INSEE, VIL_NOM, VIL_CODE_POSTAL)
+			VALUES (?, ?, ?)
+		""", (vil_code_insee, vil_nom, vil_code_postal))
+
+		# Insérer les données dans la table des adresses
+		cursor.execute("""
+			INSERT OR IGNORE INTO ADRESSES (ADR_ID, ADR_NUMERO, ADR_REP, ADR_NOM_VOIE, ADR_LATITUDE, ADR_LONGITUDE, ADR_ALTITUDE, ADR_VIL_CODE_INSEE)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+		""", (adr_id, adr_numero, adr_rep, adr_nom_voie, adr_latitude, adr_longitude, adr_altitude, vil_code_insee))
+
+		# Validation de la ville en cours
+		if vil_code_insee != ville_en_cours:
+			ville_en_cours = vil_code_insee
+			tqdm.write(Fore.BLUE + f"[*] Ville terminée : {vil_nom} ({vil_code_postal})" + Style.RESET_ALL)
+			conn.commit()
+
+	# Valider les modifications
+	conn.commit()
+
+	print(Fore.GREEN + "[+] Base de données remplie avec succès." + Style.RESET_ALL)
+
+def recupAltitude(latitude: float, longitude: float) -> float:
 	"""
 	Utilise l'API de Google pour récupérer l'élévation.
 	"""
+	try:
+		altitude = gmaps.elevation((latitude, longitude))
+		return altitude[0]["elevation"]
+	except Exception as e:
+		print(Fore.RED + f"[!] Erreur lors de la récupération de l'altitude: {e}." + Style.RESET_ALL)
+		return None
 
 if __name__ == "__main__":
+	# Vérifier si le fichier CSV existe
+	if not os.path.exists(f"adresses-{scope}.csv"):
+		recupCSV()
+
 	# Créer la base si elle n'existe pas
-	if not os.path.exists("adresses.sqlite")
-		creerBase()
+	if not os.path.exists(f"adresses-{scope}.sqlite"):
+		creerBDD()
+
+	# Remplir la base de données
+	remplirBDD()
--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # AdressesFrance
-Créer une base de données des adresses de toute la France
 
-Fichier CSV à télécharger à l'adresse :
-https://adresse.data.gouv.fr/data/ban/adresses/latest/csv/adresses-france.csv.gz
+Créer une base de données des adresses de toute la France.
+
+Fichiers CSV télécharger par le script depuis l'adresse :
+https://adresse.data.gouv.fr/data/ban/adresses/latest/csv
+
+Documentation sur le format des fichiers CSV :
+https://github.com/BaseAdresseNationale/adresse.data.gouv.fr/blob/master/public/schemas/adresses-csv.md
+
+# Fonctionnement
+
+## Création de l'environnement virtuel
+
+Vous pouvez créer votre environnement virtuel avec Python :
+
+**Sous Linux ou macOS :**
+
+```bash
+python3 -m venv .venv
+```
+
+**Sous Windows :**
+
+```powershell
+python -m venv .venv
+```
+
+## Basculer dans votre nouvel environnement virtuel
+
+**Sous Linux ou macOS :**
+
+```bash
+source .venv/bin/activate
+```
+
+**Sous Windows :**
+
+ - Dans **PowerShell** :
+	```powershell
+	.\.venv\Scripts\Activate.ps1
+	```
+
+ - Dans l'**Invite de commandes** :
+	```batch
+	.venv\Scripts\activate.bat
+	```
+## Installer les bibliothèques
+
+Vous allez avoir besoin de plusieurs bibliothèques. Vous pouvez les Installer dans votre environnement virtuel avec **pip** :
+
+```
+pip install --requirement requirements.txt
+```
+
+## Spécifiction des variables d'environnement
+
+Vous avez besoin de deux variables d'environnement à place dans un fichier ``.env`` à la racine du dépôt :
+
+ - **``GOOGLE_API_KEY``** : Votre clé d'[API Google Maps Elevation](https://developers.google.com/maps/documentation/elevation/get-api-key).
+ - **``SCOPE``** : Le numéro du département à traiter. Mettre ``france`` pour toute la France.
