c446456cb69368231df2774d4d978671fd1abf2e
ILPlais <lionel.plais@ilp-web.net>
2025-02-22 14:38:06 +0100

    Corp du projet

---
+++ BaseFrance.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import os
+import sqlite3
+import csv
+
+def creerBDD():
+	"""
+	Créer la base de données pour les adresses de toute la France.
+	"""
+
+
+def remplirBDD():
+	"""
+	Remplit la base de données avec les adresses contenues dans le fichier CSV.
+	"""
+
+def recupAltitude():
+	"""
+	Utilise l'API de Google pour récupérer l'élévation.
+	"""
+
+if __name__ == "__main__":
+	# Créer la base si elle n'existe pas
+	if not os.path.exists("adresses.sqlite")
+		creerBase()
